972e1f751e12da19f8f7205094af319314df470d
John Cowie <john.a.cowie@gmail.com>
2012-11-24 23:32:26 +0000

    updated noir version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject sandpit "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.3.0"]
-                           [noir "1.2.1"]]
+                           [noir "1.2.2-SNAPSHOT"]]
             :main sandpit.server
 			:plugins [[lein-ring "0.7.5"]] 
 			:ring {:handler sandpit.server/handler} )
