16af47d1c151e38c1a6b30af155a887d0b3a3e98
John Cowie <john.a.cowie@gmail.com>
2012-11-25 11:31:32 +0000

    Tried to define an index page

--- src/sandpit/views/welcome.clj
+++ src/sandpit/views/welcome.clj
@@ -1,9 +1,18 @@
 (ns sandpit.views.welcome
-  (:require [sandpit.views.common :as common]
-            [noir.content.getting-started])
+  (:require [sandpit.views.common :as common])
   (:use [noir.core :only [defpage]]
         [hiccup.core :only [html]]))
 
 (defpage "/welcome" []
          (common/layout
            [:p "Welcome to sandpit"]))
+
+(defpage "/" []             
+		(html5
+            [:head
+               [:title "My Noir WebPage"]
+               (include-css "/css/reset.css")]
+              [:body
+               [:div#wrapper
+                [:h1 "Hello World!"]
+				[:p "It works.  Here is some more text.  Fa la la la]])))
