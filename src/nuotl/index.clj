193e3ce9ec5f521468b0ab66b056f3d9afbc1183
John Cowie <john.a.cowie@gmail.com>
2013-01-06 21:39:39 +0000

    Added lighter coloured default rows and profile pics

--- src/nuotl/index.clj
+++ src/nuotl/index.clj
@@ -1,4 +1,5 @@
 (ns nuotl.index
+  (:require [clj-time.core :as time])
   (:use
        [compojure.core]
        [compojure.route :only [not-found resources]]
@@ -7,11 +8,16 @@
         [hiccup.page :only [include-css html5]]
         [nuotl.dao :only [get-events]]
         [clj-time.format :only [unparse formatter]]
-        [clj-time.core :only [now year month]]
         [nuotl.events :only [to-month]]
+        [ring.util.response :only [redirect]]
         ))
 
 (defn format-date [date] (unparse (formatter "HH:mm") date))
+
+(defn get-profile-pic-url [tweeter]
+  (format
+   "https://api.twitter.com/1/users/profile_image?screen_name=%s&size=bigger"
+   (tweeter :name)))
 
 (defn event-row [event]
                  [:tr {:class (format "event %s" (event :tags))}
@@ -19,6 +25,9 @@
                    [:td {:class "time"} (format-date (event :end))]
                    [:td {:class "text"} (event :text)]
                    [:td {:class "name"} ((event :tweeter) :display-name)]
+                   [:td
+                     [:img {:src (get-profile-pic-url (event :tweeter))}]
+                   ]
                   ])
 
 (defn day-table [day events]
@@ -28,20 +37,33 @@
                         (for [event events]
                                 (event-row event))])
 
+(defn get-relative-month-url [y m diff]
+  (let [rel-month (time/plus (time/date-time y m 10 0 0 0) (time/months diff))]
+    (let [year (time/year rel-month) month (time/month rel-month)]
+      (format "/events/%s/%s" year month))))
 
 (defn event-page [y m]
-   (let [month-map (to-month (get-events (read-string y) (read-string m)) (read-string y) (read-string m))]
-                (html5
-            [:head
-               [:title (format "Next Up On The Left %s %s" y m)]
-               (include-css "/css/reset.css")]
-            [:body
-               [:div {:class "container"}
-                        [:h1 (format "%s/%s" y m)]
-                 (for [[day events] month-map]
-                                        (day-table day events))]])))
+  (let [yr (read-string y) mth (read-string m)]
+    (let [month-map (to-month (get-events yr mth) yr mth)]
+      (html5
+       [:head
+        [:title (format "Next Up On The Left %s %s" y m)]
+        (include-css "/css/reset.css")]
+       [:body
+        [:div {:class "container"}
+         [:a {:href (get-relative-month-url yr mth -1)} "Previous"]
+         [:a {:href (get-relative-month-url yr mth +1)} "Next"]
+         [:h1 (format "%s/%s" y m)]
+         (for [[day events] month-map]
+           (day-table day events))]]))))
+
+(defn current-month-url []
+  (let [n (time/now)]
+    (format "/events/%s/%s" (time/year n) (time/month n))))
+
 
 (defroutes app-routes
+  (GET "/" []  (redirect (current-month-url)))
   (GET "/events/:y/:m" [y m] (event-page y m))
   (resources "/")
   (not-found "These aren't the droids you are looking for..."))
