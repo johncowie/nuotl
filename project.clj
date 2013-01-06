56f3e9ead3c8372143554d08d7313ae40fac9431
John Cowie <john.a.cowie@gmail.com>
2013-01-06 16:09:47 +0000

    Removed noir from project

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
 (defproject sandpit "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
-                           [noir "1.2.2"]
+                           [hiccup "1.0.2"]
                            [com.novemberain/monger "1.4.0"]
                            [clj-time "0.4.4"]
-                           [cheshire "5.0.0"]]
+                           [cheshire "5.0.0"]
+                           [compojure "1.1.3"]
+                           [enlive "1.0.1"]]
             :main sandpit.server
-			:plugins [[lein-ring "0.7.5"]] 
-			:ring {:handler sandpit.server/handler} )
+                        :plugins [[lein-ring "0.7.5"]]
+                        :ring {:handler sandpit.views.welcome/app})
--- src/sandpit/views/welcome.clj
+++ src/sandpit/views/welcome.clj
@@ -1,9 +1,10 @@
 (ns sandpit.views.welcome
-  (:use [noir.core :only [defpage defpartial]]
-        [noir.response :only [redirect]]
-        [noir.util.test :only [send-request]]
+  (:use
+       [compojure.core]
+       [compojure.route :only [not-found resources]]
+       [compojure.handler :only [site]]
         [hiccup.core :only [html]]
-		[hiccup.page-helpers :only [include-css html5]]
+        [hiccup.page :only [include-css html5]]
         [sandpit.dao.dao :only [get-events]]
         [clj-time.format :only [unparse formatter]]
         [clj-time.core :only [now year month]]
@@ -12,7 +13,7 @@
 
 (defn format-date [date] (unparse (formatter "HH:mm") date))
 
-(defpartial event-row [event]
+(defn event-row [event]
                  [:tr {:class (format "event %s" (event :tags))}
                    [:td {:class "time"} (format-date (event :start))]
                    [:td {:class "time"} (format-date (event :end))]
@@ -20,38 +21,30 @@
                    [:td {:class "name"} ((event :tweeter) :display-name)]
                   ])
 
-(defpartial day-table [day events]
-  		[:table 
-          [:tr {:class "header"} 
-           [:th {:colspan 4} day]] 
-			(for [event events]
-				(event-row event))])
+(defn day-table [day events]
+                [:table
+          [:tr {:class "header"}
+           [:th {:colspan 4} day]]
+                        (for [event events]
+                                (event-row event))])
 
 
-(defpartial event-page [y m]
+(defn event-page [y m]
    (let [month-map (to-month (get-events (read-string y) (read-string m)) (read-string y) (read-string m))]
-		(html5
+                (html5
             [:head
                [:title (format "Next Up On The Left %s %s" y m)]
                (include-css "/css/reset.css")]
             [:body
                [:div {:class "container"}
-                	[:h1 (format "%s/%s" y m)]
+                        [:h1 (format "%s/%s" y m)]
                  (for [[day events] month-map]
-					(day-table day events))]])))
+                                        (day-table day events))]])))
 
-(defpage "/events/:y/:m" {:keys [y m]}
-	(event-page y m))
+(defroutes app-routes
+  (GET "/events/:y/:m" [y m] (event-page y m))
+  (resources "/")
+  (not-found "These aren't the droids you are looking for..."))
 
-(defpage "/" {}
-	(redirect (format "/events/%s/%s" (year (now)) (month (now)))))
-
-
-
-
-
-
-
-
-
-
+(def app
+  (site app-routes))
