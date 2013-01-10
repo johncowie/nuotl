27ba1c1bdf367cad0b69b9f63055a796a1d8a06a
John Cowie <john.a.cowie@gmail.com>
2013-01-10 08:37:20 +0000

    Added function for retrieving feature requests

--- src/nuotl/dao.clj
+++ src/nuotl/dao.clj
@@ -2,8 +2,8 @@
   (:require
     [monger.core :as mg]
     [monger.collection :as mc]
-   	[monger.joda-time]
-   	[nuotl.test-data :as test-data])
+        [monger.joda-time]
+        [nuotl.test-data :as test-data])
   (:use [clj-time.core :only [date-time plus months now year month]]
         [nuotl.events :only [to-month]]
         [monger.operators]))
@@ -12,11 +12,11 @@
 (mg/set-db! (mg/get-db "nuotl"))
 
 (defn split-event [event]
-	{:event (assoc event :tweeter ((event :tweeter) :_id)) :tweeter (event :tweeter)})
+        {:event (assoc event :tweeter ((event :tweeter) :_id)) :tweeter (event :tweeter)})
 
 (defn add-event [event]
   (let [components (split-event event)]
-	(mc/save "event" (components :event))
+        (mc/save "event" (components :event))
     (mc/save "tweeter" (components :tweeter))))
 
 (defn get-event [id]
@@ -24,11 +24,13 @@
 
 (defn get-events [y m]
   (let [start-date (date-time y m) end-date (plus (date-time y m) (months 1))]
-  	(let [events (mc/find-maps "event" {:start {$gte start-date $lt end-date}}) updated-events (transient [])]
+        (let [events (mc/find-maps "event" {:start {$gte start-date $lt end-date}}) updated-events (transient [])]
       (doseq [event events]
         (conj! updated-events (assoc event :tweeter (mc/find-one-as-map "tweeter" {:_id (event :tweeter)}))))
-	  (persistent! updated-events))))
+      (persistent! updated-events))))
 
+(defn get-features []
+  (sort-by #(% :created-at) (mc/find-maps "feature" {})))
 
 (for [e test-data/data]
   (add-event e))
