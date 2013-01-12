a8eb497cbff962682405faf205ebce22fee70f7c
John Cowie <john.a.cowie@gmail.com>
2013-01-12 20:41:22 +0000

    Did some refactoring

--- src/nuotl/index.clj
+++ src/nuotl/index.clj
@@ -11,7 +11,9 @@
         [hiccup.page :only [include-css html5]]
         [hiccup.util :only [url with-base-url]]
         [hiccup.middleware :only [wrap-base-url]]
+        [nuotl.middleware :only [request-printer wrap-uri-prefix]]
         [nuotl.dao :only [get-events get-features]]
+        [nuotl.date-helper :only [month-name day-name get-int-suffix]]
         [clj-time.format :only [unparse formatter]]
         [nuotl.events :only [to-month]]
         [ring.middleware.resource :only [wrap-resource]]
@@ -43,38 +45,6 @@
                    ]
                   ])
 
-(def day-names {1 "Monday"
-                2 "Tuesday"
-                3 "Wednesday"
-                4 "Thursday"
-                5 "Friday"
-                6 "Saturday"
-                7 "Sunday"
-                })
-
-(def month-names {1 "January"
-                  2 "February"
-                  3 "March"
-                  4 "April"
-                  5 "May"
-                  6 "June"
-                  7 "July"
-                  8 "August"
-                  9 "September"
-                  10 "October"
-                  11 "November"
-                  12 "December"
-                  })
-
-(defn day-name [d m y]
-  (day-names (time/day-of-week (time/date-time y m d 12 0 0))))
-
-(defn get-int-suffix [i]
-  (case (rem i 100)
-    (11 12 13) "th"
-    (case (rem i 10)
-      1 "st" 2 "nd" 3 "rd" "th")))
-
 (defn day-table [day month year events]
                 [:table
           [:tr {:class "header"}
@@ -88,108 +58,72 @@
     (let [year (time/year rel-month) month (time/month rel-month)]
       (url (format "/events/%s/%s" year month)))))
 
+(defn page-container [title & content]
+  (ring-response/content-type
+   (ring-response/response
+    (html5
+     [:head
+      [:title (format "0.1.0 ALPHA - %s" title)]
+      (include-css "/css/reset.css")]
+     [:body
+      [:div {:class "container"}
+       [:a {:href (url "/")} "Events"]
+       " " [:a {:href (url "/features")} "Feature Requests"]
+       " " [:a {:href (url "/releases")} "Releases"]
+       " " [:a {:href (url "/instructions")} "Instructions"]
+       content]])) "text/html"))
+
 (defn event-page [y m]
   (let [yr (read-string y) mth (read-string m)]
     (let [month-map (to-month (get-events yr mth) yr mth)]
-      (html5
-       [:head
-        [:title (format "0.1.0 ALPHA - Next Up On The Left %s %s" y m)]
-        (include-css "/css/reset.css")]
-       [:body
-        [:div {:class "container"}
-         [:a {:href (url "/features")} "Feature Requests"]
-         " " [:a {:href (url "/releases")} "Releases"]
-         " " [:a {:href (url "/instructions")} "Instructions"]
-         [:h1 (format "%s %s" (month-names mth) y)]
-         [:a {:href (get-relative-month-url yr mth -1)} "Previous"]
-         " "
-         [:a {:href (get-relative-month-url yr mth +1)} "Next"]
-         (for [[day events] month-map]
-           (day-table day mth yr events))]]))))
+      (page-container "Next Up On The Left"
+       [:h1 (format "%s %s" (month-name mth) y)]
+       [:a {:href (get-relative-month-url yr mth -1)} "Previous"]
+       " "
+       [:a {:href (get-relative-month-url yr mth +1)} "Next"]
+       (for [[day events] month-map]
+         (day-table day mth yr events))))))
 
 (defn feature-page []
-  (html5
-   [:head
-    [:title "Feature Requests"]
-    (include-css "/css/reset.css")]
-   [:body
-    [:div {:class "container"}
-     [:h1 "Feature Requests"]
-     [:table
-      (for [feature (get-features)]
-        [:tr
-         [:td (unparse (formatter "dd/MM/yyyy HH:mm") (feature :created-at))]
-         [:td (format "@%s"(feature :username))]
-         [:td (feature :text)]])]]]))
+  (page-container "Feature Requests"
+   [:h1 "Feature Requests"]
+   [:table
+    (for [feature (get-features)]
+      [:tr
+       [:td (unparse (formatter "dd/MM/yyyy HH:mm") (feature :created-at))]
+       [:td (format "@%s"(feature :username))]
+       [:td (feature :text)]])]))
+
+(def release-html (slurp (clojure.java.io/resource "public/templates/releases.html")))
+(def instructions-html (slurp (clojure.java.io/resource "public/templates/instructions.html")))
 
 (defn release-page []
-  (html5
-   [:head
-    [:title "Releases"]
-    (include-css "/css/reset.css")]
-   [:body
-    [:div {:class "container"}
-     [:h1 "Releases"]
-     (slurp (clojure.java.io/resource "public/templates/releases.html"))
-     ]]))
+  (page-container "Releases"
+   [:h1 "Releases"]
+   release-html ))
 
 (defn instructions-page []
-  (html5
-   [:head
-    [:title "Instructions"]
-    (include-css "/css/reset.css")
-    ]
-   [:body
-    (slurp (clojure.java.io/resource "public/templates/instructions.html"))]))
+  (page-container "Instructions"
+   instructions-html))
 
-(def url-context (atom "bob"))
-
-(defn current-month-url []
+(defn redirect-to-current-month []
   (let [n (time/now)]
-    (format "/events/%s/%s" (time/year n) (time/month n))))
+    (ring-response/redirect
+     (format "/events/%s/%s" (time/year n) (time/month n)))))
 
 (defroutes app-routes
-  (GET "/" []  (ring-response/redirect (current-month-url)))
-  (GET "/events/:y/:m" [y m]
-       (ring-response/content-type (ring-response/response (event-page y m)) "text/html"))
-  (GET "/features" []
-       (ring-response/content-type (ring-response/response (feature-page)) "text/html"))
-  (GET "/releases" []
-       (ring-response/content-type (ring-response/response (release-page)) "text/html"))
-  (GET "/instructions" []
-       (ring-response/content-type (ring-response/response (instructions-page)) "text/html"))
+  (GET "/" []  (redirect-to-current-month))
+  (GET "/events/:y/:m" [y m] (event-page y m))
+  (GET "/features" [] (feature-page))
+  (GET "/releases" [] (release-page))
+  (GET "/instructions" [] (instructions-page))
   (resources "/")
   (not-found "These aren't the droids you are looking for..."))
-
-(defn request-printer [handler]
-  (fn [request]
-    (println (format "INCOMING REQUEST: %s" request))
-    (println (format "SERVER: %s:%s" (request :server-name) (request :server-port)))
-    (handler request)))
-
-
-(defn wrap-uri-prefix [handler prefix]
-  (fn [request]
-    (let [response (handler (assoc request
-                              :uri (clojure.string/replace-first (:uri request)
-                                                                 (re-pattern (str "^" prefix "/?"))
-                                                                 "/")))]
-      (if (<= 300 (:status response) 308) ; Only rewrite redirects
-        (assoc response
-          :headers (assoc (:headers response)
-                     "Location" (clojure.string/replace-first (get-in response [:headers "Location"])
-                                                              #"^/"
-                                                              (str prefix "/"))))
-        response))))
 
 (def app
   (->
    (site app-routes)
-   (wrap-resource "/public")
-   ;(wrap-base-url "/nuotl")
-   ;(wrap-uri-prefix "/nuotl")
-   (request-printer)
-   ))
+   (wrap-resource "/public")))
 
 (defn -main [& args]
   (if (not (empty? args))
