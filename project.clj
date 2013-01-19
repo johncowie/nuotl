9bde5f97fa6b091b3a4dc81d69333713bd27a1b4
John Cowie <john.a.cowie@gmail.com>
2013-01-19 09:53:14 +0000

    Releasing 0.1.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nuotl "0.1.1"
+(defproject nuotl "0.1.2"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [hiccup "1.0.2"]
